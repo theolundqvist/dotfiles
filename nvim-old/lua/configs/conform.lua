69bb34a01b2d52bc6d905116e981e80ce7b6120f
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-10-16 20:08:27 +0200

    add nvim-old config

---
+++ nvim-old/lua/configs/conform.lua
@@ -0,0 +1,23 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettier" },
+    html = { "prettier" },
+    js = { "prettier" },
+    jsx = { "prettier" },
+    ts = { "eslint" },
+    tsx = { "eslint" },
+    go = { "gofmt" },
+    wsgi = { "black" },
+    py = { "black" }
+    bib = {"ltex"}
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+require("conform").setup(options)
