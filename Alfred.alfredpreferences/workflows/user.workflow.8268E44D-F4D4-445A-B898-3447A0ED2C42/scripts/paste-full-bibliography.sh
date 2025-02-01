6ff8f73503a42ee5f2ac7a1e2b9c12f9f7ce6d13
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2025-02-01 12:51:06 +0000

    alfred stuf

---
+++ Alfred.alfredpreferences/workflows/user.workflow.8268E44D-F4D4-445A-B898-3447A0ED2C42/scripts/paste-full-bibliography.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+# shellcheck disable=SC2154
+export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
+#───────────────────────────────────────────────────────────────────────────────
+
+# GUARD
+if ! command -v pandoc &>/dev/null; then
+	echo -n "You need to install \`pandoc\` for this feature."
+	return 1
+fi
+
+citekey="$*"
+csl=$([[ -f "$csl_for_pandoc" ]] && echo "$csl_for_pandoc" || echo "./support/apa-7th.csl")
+library="$bibtex_library_path"
+
+dummydoc="---
+nocite: '@$citekey'
+---"
+
+reference=$(echo -n "$dummydoc" |
+	command pandoc --citeproc --read=markdown --write=plain --wrap=none \
+	--csl="$csl" --bibliography="$library" 2>&1)
+
+echo -n "$reference"
